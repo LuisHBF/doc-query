93d9861adb352f8ccc5abe9daf1cb4083177bb2a
Luis Henrique Buzzo Franco <luishbfranco@gmail.com>
2026-03-22 13:12:53 -0300

    chore: add Application classes and application.yml for all modules

---
+++ embedding-service/src/main/java/br/com/docquery/embedding/EmbeddingServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.docquery.embedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddingServiceApplication.class, args);
+    }
+
+}
